8e05ed556f3a573f0075f96d1bbf21deb07bd3c3
taptap <alexey.e.egorov@gmail.com>
2013-02-09 23:38:09 +0400

    switched queue to list implementation

--- src/message_queue.erl
+++ src/message_queue.erl
@@ -2,34 +2,42 @@
 -export([new/0, push/2, push_back/2, pop/1, pop_back/1]).
 -export([print/1]).
 
+-define(MAXLENGTH, 5).
+
 new() ->
     [].
 
 push(Element, Queue) when is_list(Element) ->
     Length = length(Queue),
-    if Length < 20 ->
-	    [[Element]|Queue];
-       Length >= 20 ->
-	    [H|T] = Queue,
-	    
+    if Length < ?MAXLENGTH ->
+	    [Element|Queue];
+       Length >= ?MAXLENGTH ->
+	    [_|Head] = lists:reverse(Queue),
+	    [Element|lists:reverse(Head)]
     end.
 	    
 push_back(Element, Queue) when is_list(Element) ->
     Length = length(Queue),
-    if Length < 20 ->
+    if Length < ?MAXLENGTH ->
 	    [Queue|[Element]];
-       Length >= 20 ->
-	    {_, Q} = queue:out(Queue),
-	    queue:in_r(Element, Q)
+       Length >= ?MAXLENGTH ->
+	    [_|Tail] = Queue,
+	    [Tail|[Element]]
     end.
 
+pop([]) ->
+    {empty, []};
 pop(Queue) ->
-    queue:out(Queue).
+    [H|T] = Queue,
+    {H,T}.
 
+pop_back([]) ->
+    {empty, []};
 pop_back(Queue) ->
-    queue:out_r(Queue).
+    [H|T] = lists:reverse(Queue),
+    {H,lists:reverse(T)}.
 
 print(Queue) ->
-    queue:filter(fun(S) -> io:format("~p, ", [S]), true end, Queue),
+    lists:map(fun(S) -> io:format("~p, ", [S]) end, Queue),
     ok.
 			 
