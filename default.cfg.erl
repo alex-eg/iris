a6bfa0bd3734e5cb61bd1031d255b647b5f62435
taptap <alexey.e.egorov@gmail.com>
2013-02-10 23:08:02 +0400

    added default config

---
+++ default.cfg.erl
@@ -0,0 +1,11 @@
+{config, [
+	  {port, 5222},
+	  {server, "example.net"},
+	  {jid, "somejid"},
+	  {password, "secret_password"},
+	  {room, "room"},
+	  {timeout, 1500},
+	  {nick, "somenick"},
+	  {modules, [last, exit]},
+	  {status, "whatever_status"}
+]}.
