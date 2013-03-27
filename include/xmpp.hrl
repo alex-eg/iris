72705c448f481feb7a9a3477505774b11d6b3007
taptap <alexey.e.egorov@gmail.com>
2013-03-27 20:09:01 +0400

    renamed configuration record

--- include/xmpp.hrl
+++ include/xmpp.hrl
@@ -1,15 +1,17 @@
 -include_lib("exmpp/include/exmpp.hrl").
 
--record(bot_info,
+-record(jid_info,
 	{port = 5222,
 	 server_address,
 	 jid,
 	 password,
 	 room,
 	 nick,
+	 resource,
 	 status,
 	 timeout = 10000,
-	 modules = []
+	 modules = [],
+	 roster = []
 	}).
 
 -record(connection_info,
@@ -17,16 +19,15 @@
 	 jid,
 	 password,
 	 status,
-	 resource,
 	 modules = []
-	 }).
+	}).
 
 -record(room_info,
 	{jid,
 	 password = none,
 	 nick,
 	 modules = []
-	 }).
+	}).
 
 
 -define(EMPTY_PRESENCE, #xmlel{name = 'presence'}).
