def8548745862ce03224d33cfb926f13bb573fc0
taptap <alexey.e.egorov@gmail.com>
2013-04-01 14:48:25 +0400

    joining password-protected chat now works

--- src/muc_tools.erl
+++ src/muc_tools.erl
@@ -6,21 +6,12 @@
 
 join_groupchat(Session, RoomTuple) ->
     {Room, Nick, Password} = RoomTuple,
-    join_groupchat(Session, Room, Nick, Password).
-
-join_groupchat(Session, Room, Nick, nopassword) ->
     ulog:info("Joining ~s as ~s", [Room, Nick]),
-    Presence = create_basic_presence(Room, Nick),
-    exmpp_session:send_packet(Session, Presence);
-join_groupchat(Session, Room, Nick, Password) ->
-    BasePresence = create_basic_presence(Room, Nick),
-    Presence = exmpp_xml:append_child(BasePresence#xmlel{name = x},
-				      #xmlel{name = password,
-					     children = [#xmlcdata{cdata = Password}]}),
+    Presence = create_presence(Room, Nick, Password),
     exmpp_session:send_packet(Session, Presence).
     
 %% Helpers below
-create_basic_presence(Room, Nick) ->
+create_presence(Room, Nick, nopassword) ->
     %% Fixme: self-defined marco and manual presence creation is definetly NOT a good practice
     %% Well, exmpp seems to provide no such functionality, so I have to prettify it by myself
     BasePresence = exmpp_xml:set_attribute(?EMPTY_PRESENCE,
@@ -31,7 +22,18 @@ create_basic_presence(Room, Nick) ->
 					     attrs = [#xmlattr{name = <<"xmlns">>, 
 							       value = ?NS_MUC_b}]
 					    }
+				     );
+create_presence(Room, Nick, Password) ->
+    BasePresence = exmpp_xml:set_attribute(?EMPTY_PRESENCE,
+					   <<"to">>,
+					   list_to_binary(Room ++ "/" ++ Nick)),
+    Children = #xmlel{name = password,
+		      children = [#xmlcdata{cdata = Password}]},
+    Presence = exmpp_xml:append_child(BasePresence,
+				      #xmlel{name = x, 
+					     attrs = [#xmlattr{name = <<"xmlns">>, 
+							       value = ?NS_MUC_b}],
+					     children = [Children]
+					    }
 				     ).
-
-
     
