176303e083d0306a4ead2dee1f9a96b493b729cf
taptap <alexey.e.egorov@gmail.com>
2013-04-11 20:47:56 +0400

    fixed special symbol escaping, added "not found" resut processing

--- src/modules/g.erl
+++ src/modules/g.erl
@@ -6,18 +6,23 @@ run("") ->
 run(Args) ->
     [{google_search, SearchConfig}] = gen_server:call(root, {get_config, google_search}),
     
-    Query = edoc_lib:escape_uri(Args),
+    EscapedQuery = http_uri:encode(Args),
+    Query = re:replace(EscapedQuery, "%20", "+", [global, unicode, {return, list}]),
     ApiKey = proplists:get_value(api_key, SearchConfig),
     EngineId = proplists:get_value(engine_id, SearchConfig),
     QueryURL = "https://www.googleapis.com/customsearch/v1?key="
     	++ ApiKey
     	++ "&cx=" ++ EngineId
     	++ "&q=" ++ Query
-	++ "&num=1",				% We need only 1st result
+    	++ "&num=1",				% We need only 1st result
 
     ResponseJSON = gen_server:call(root, {get_http, QueryURL}),
     {Response} = jsonx:decode(list_to_binary(ResponseJSON)),
-    {<<"items">>, ResultList} = lists:keyfind(<<"items">>, 1, Response),
+    Items = lists:keyfind(<<"items">>, 1, Response),
+    Result = extract_result(Items),
+    Result.
+
+extract_result({<<"items">>, ResultList}) ->
     [{FirstResult}|_] = ResultList,
     ResultProplist = lists:map(fun(Entry) ->
     				       {Key, Value} = Entry,
@@ -31,6 +36,11 @@ run(Args) ->
     Snippet = proplists:get_value(snippet, ResultProplist),
     Link = proplists:get_value(link, ResultProplist),
     binary_to_list(Title) ++ "\n" ++
-	binary_to_list(Snippet) ++ "\n" ++
-	binary_to_list(Link).
+    	binary_to_list(Snippet) ++ "\n" ++
+    	binary_to_list(Link);
+extract_result(false) ->
+    "Sorry, nothing found".
+    
+
+
     
