310221c952cfba15fe8d42779f2184196b193a9c
taptap <alexey.e.egorov@gmail.com>
2013-04-23 10:24:54 +0400

    snafu

--- src/plugins/external_interface.erl
+++ src/plugins/external_interface.erl
@@ -33,9 +33,9 @@ handle_call(Any, _From, State) ->
     ulog:info("Recieved UNKNOWN request: ~p", [Any]),
     {noreply, State}.
 
-handle_cast({subscribe, From}, State) ->
-    proc_lib:spawn(?MODULE, accept_loop, [self(), LSocket, From]),
-    {noreply, State};
+%handle_cast({subscribe, From}, State) ->
+%    proc_lib:spawn(?MODULE, accept_loop, [self(), LSocket, From]),
+%    {noreply, State};
 handle_cast({accepted, _Pid, Subscriber}, State = #state{socket = LSocket}) ->
     proc_lib:spawn(?MODULE, accept_loop, [self(), LSocket, Subscriber]),
     {noreply, State};
@@ -66,7 +66,7 @@ loop(Socket, Subscriber) ->
         {ok, Data} ->
 	    ulog:info("Recieved data via socket: ~p", [Data]),
 	    gen_server:cast(Subscriber, {socket_recieved, Data}),
-            loop(Socket, Subscriber)
+            loop(Socket, Subscriber);
         {error, closed} ->
             ok
     end.
