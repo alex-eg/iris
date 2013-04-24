99f2d08fd40156c0e691090ff9b1bd960f692043
taptap <alexey.e.egorov@gmail.com>
2013-04-24 12:03:39 +0400

    finished word module

--- src/modules/word.erl
+++ src/modules/word.erl
@@ -6,8 +6,7 @@
 
 run("") ->
     "What is the sound of one hand clapping?";
-run(UnescapedArgs) ->
-    Args = binary_to_list(unicode:characters_to_binary(UnescapedArgs)),
+run(Args) ->
     [{denshi_jisho, Config}] = gen_server:call(root, {get_config, denshi_jisho}),
     Base = proplists:get_value(request_url, Config),
     [Head|Tail] = string:tokens(Args, " "),
@@ -32,11 +31,11 @@ shortcut(UnescapedArgs) ->
 make_request_url("en", Tail, Base) ->
     Query = create_query(Tail),
     io_lib:format(Base, ["", Query]);
-make_request_url("jp", Tail, Base) ->
+make_request_url("ja", Tail, Base) ->
     Query = create_query(Tail),
     io_lib:format(Base, [Query, ""]);
 make_request_url(Something, Tail, Base) ->	% Defaulting to jp clause
-    make_request_url("jp", [Something|Tail], Base).
+    make_request_url("ja", [Something|Tail], Base).
 
 
 create_query([]) ->
@@ -59,6 +58,8 @@ extract_info(_AnythingElse) ->
 
 
 extract_words(undefined, _) ->
+    "No comments in document, w00t!";
+extract_words(<<" Found no words ">>, _) ->
     "Nothing found";
 extract_words(<<" Found words ">>, BodyTags) ->
     KanjiTags = get_tag_list(<<"td">>,
@@ -77,26 +78,54 @@ extract_words(<<" Found words ">>, BodyTags) ->
     SenseTags = get_tag_list(<<"span">>,
 			     {<<"class">>, <<"tags">>},
 			     BodyTags),
-%%    Sense = extract_sense(SenseTags),
-    io:format("KanjiTags:~n~p~nKanji:~n~p~nKana: ~p~nMean: ~p~nTags:~p~n",
-	      [KanjiTags, Kanji, KanaTags, Meaning, SenseTags]),
-    ok.
+    Sense = extract_tags(SenseTags),
 
+    KanaStringList = lists:map(fun(E) -> binary_to_list(E) end,
+			       Kana),
+    KanjiStringList = lists:map(fun(E) -> binary_to_list(E) end,
+				Kanji),
+    MeaningStringList = lists:map(fun(E) -> binary_to_list(E) end,
+				  Meaning),
+    SenseStringList = lists:map(fun binary_to_list/1, Sense),
 
--spec get_tag_list(bitstring(),
-		   {bitstring(), bitstring()},
-		   bitstring()) -> list().
+    Len = min(2, length(KanjiStringList)),
+    {Start, _} = lists:split(Len,
+			     lists:zip(lists:zip(KanjiStringList, KanaStringList), 
+				       lists:zip(MeaningStringList, SenseStringList))),
+    Result = lists:map(fun({{Kj, Kn}, {Mn, Tg}}) ->
+			       Kj1 = string:strip(Kj),
+			       Kj2 = string:strip(Kj1, right, $\t),
+
+			       Kn1 = string:strip(Kn),
+			       Kn2 = string:strip(Kn1, right, $\t),
+
+			       Mn1 = string:strip(Mn),
+			       Mn2 = string:strip(Mn1, right, $\t),
+			       Mn3 = re:replace(Mn2, ";([1-9]:)", "\n\\1", [unicode, global, {return, list}]),
+
+			       Tg1 = string:strip(Tg),
+			       Tg2 = string:strip(Tg1, right, $\t),
+			       Tg3 = string:strip(Tg2, right, $\n),
+
+			       lists:flatten(io_lib:format("~n~s [~s]~n~s~n~s~n",[Kj2, Kn2, Mn3, Tg3]))
+		       end,
+		       Start),
+    string:strip(lists:flatten(Result), right, $\n).
+
+-spec get_tag_list(binary(),
+		   {binary(), binary()},
+		   binary()) -> list().
 get_tag_list(Name, {Param, Value}, Tree) ->
     collect_tags(Name, {Param, Value}, Tree, []).
 
 
--spec collect_tags(bitstring(), 
-		   {bitstring(), bitstring()},
-		   bitstring(),
+-spec collect_tags(binary(), 
+		   {binary(), binary()},
+		   binary(),
 		   list()) -> list().
 collect_tags(_, _, [], Accum) ->
     Accum;
-collect_tags(Name, {Param, Value}, [PCDATA|Tail], Accum) when is_bitstring(PCDATA) -> %% Skip raw text
+collect_tags(Name, {Param, Value}, [PCDATA|Tail], Accum) when is_binary(PCDATA) -> %% Skip raw text
     collect_tags(Name, {Param, Value}, Tail, Accum);
 collect_tags(Name, {Param, Value}, [{comment, _}|Tail], Accum) -> %% Skip comments
     collect_tags(Name, {Param, Value}, Tail, Accum);
@@ -136,31 +165,50 @@ extract_kanji(KanjiList) ->
   
 extract_kana(KanaList) ->
     lists:map(fun(Tag) ->
-		      {_,_,[PCDATA]} = Tag,
-		      PCDATA
+		      {_,_,Children} = Tag,
+		      Kana = lists:map(fun get_pcadata/1, Children),
+		      lists:foldr(fun(Elem, Acc) ->
+					  <<Elem/binary, Acc/binary>>
+					      end,
+				  <<"">>,
+				  Kana)
 	      end,
 	      KanaList).
 
 extract_meaning(MeaningList) ->
     Meanings = lists:map( 
 		 fun(Tag) ->
-			 {_,_,Meanings} = Tag,
-			 Meanings
+			 {_,_,Children} = Tag,
+			 Meanings = lists:map(fun get_pcadata/1, Children),
+			 lists:foldr(fun(Elem, Acc) ->
+					     <<Elem/binary, Acc/binary>>
+				     end,
+				     <<"">>,
+				     Meanings)
 		 end,
 		 MeaningList),
     Meanings.    
     
+extract_tags(TagList) ->
+    Tags = lists:map(
+ 	     fun(Tag) ->
+ 		     {_, _, Children} = Tag,
+		     Tags = lists:map(fun get_pcadata/1, Children),
+		     lists:foldr(fun(Elem, Acc) ->
+					 <<Elem/binary, Acc/binary>>
+				 end,
+				 <<"">>,
+				 Tags)
+		 end,
+		 TagList),
+    Tags.
+
 
 get_pcadata(Tag) when is_binary(Tag) ->
     Tag;
 get_pcadata([]) ->
     <<"">>;
-get_pcadata({Tag, Attributes, [Child]}) when is_bitstring(Tag) ->
-    get_pcadata(Child).
-    
-
-
-
-
-			  
-    
+get_pcadata({Tag, _Attributes, []}) when is_bitstring(Tag) ->
+    <<"">>;
+get_pcadata({Tag, _Attributes, [Child]}) when is_bitstring(Tag) ->
+    get_pcadata(Child).			  
