ccac3f6d1946393307cea8f8e2473e17daca05b1
taptap <alexey.e.egorov@gmail.com>
2013-09-03 21:25:41 +0400

    add: debug output

--- src/modules/g.erl
+++ src/modules/g.erl
@@ -16,7 +16,6 @@ run(Args) ->
     	++ "&cx=" ++ EngineId
     	++ "&q=" ++ Query
     	++ "&num=1",				% We need only 1st result
-    io:format("~p~n", gen_server:call(root, {get_http, QueryURL})),
     {{_, 200, _}, _, ResponseJSON} = gen_server:call(root, {get_http, QueryURL}),
     {Response} = jsonx:decode(list_to_binary(ResponseJSON)),
     Items = lists:keyfind(<<"items">>, 1, Response),
@@ -32,15 +31,15 @@ extract_result({<<"items">>, ResultList}) ->
     				       {AtomKey, Value}
     			       end,
     			       FirstResult),
-
     Title = proplists:get_value(title, ResultProplist),
     Snippet = proplists:get_value(snippet, ResultProplist),
     Link = proplists:get_value(link, ResultProplist),
+    ulog:debug("Title:   ~p", [Title]),
+    ulog:debug("Snippet: ~p", [Snippet]),
+    ulog:debug("Link:    ~p", [Link]),
     Result = binary_to_list(Title) ++ "\n" ++
     	binary_to_list(Snippet) ++ "\n" ++
     	binary_to_list(Link),
-    ulog:debug("Stairs are: ~s", [Result]),
-    io:format("Stairs are unicode: ~s~n", [Result]),
     Result;
 extract_result(false) ->
     "Sorry, nothing found".
