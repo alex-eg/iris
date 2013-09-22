fe815b4c87b31049758040acee090a7f4b6b9b8a
taptap <alexey.e.egorov@gmail.com>
2013-09-22 13:52:28 +0400

    fix qw module, now it works properly

--- src/modules/qw.erl
+++ src/modules/qw.erl
@@ -1,29 +1,47 @@
 -module(qw).
--export([run/1]).
--behavior(iris_module).
+-export([run/1, qw/1, qw/5]).
+%-behavior(iris_module).
 
 run("") ->
-    qw(jid_worker:get_message("channel@conference.coderollers.com/ktt9", 1)).
+    LastMessage = jid_worker:get_message("channel@conference.coderollers.com/ktt9", 1),
+    List = qw(LastMessage),
+    List.
 
 qw("") ->
     "";
 qw(String) ->
-    En = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVfBNM<>?",
-    Ru = "йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,",
-    [H|_] = String,
+    UnicodeString = unicode:characters_to_list(list_to_binary(String)),
+    En = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?",
+    Ru = unicode:characters_to_list(<<"йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,">>),
+    [H|_] = UnicodeString,
     InEnglish = lists:member(H, En), %% if true, performing English to Russian conversion
-    qw(String, InEnglish, En, Ru, "").
+    %% Suddenly! Lisp 
+    binary_to_list(
+      unicode:characters_to_binary(
+        qw(UnicodeString, InEnglish, En, Ru, ""))).
 
 qw("", _, _, _, Acc) ->
-    Acc;
+    lists:reverse(Acc);
 qw([First|Second], true, En, Ru, Acc) ->
-    qw(Second, true, En, Ru, [lists:nth(index_of(First, En), Ru)|Acc]);
+    Index = index_of(First, En),
+    case Index of 
+        Num when is_integer(Num) ->
+            qw(Second, true, En, Ru, [lists:nth(Num, Ru)|Acc]);
+        not_found ->
+            qw(Second, true, En, Ru, [First|Acc])
+    end;
 qw([First|Second], false, En, Ru, Acc) ->
-    qw(Second, true, En, Ru, [lists:nth(index_of(First, Ru), En)|Acc]).
+    Index = index_of(First, Ru),
+    case Index of 
+        Num when is_integer(Num) ->
+            qw(Second, false, En, Ru, [lists:nth(Num, En)|Acc]);
+        not_found ->
+            qw(Second, false, En, Ru, [First|Acc])
+    end.
 
 index_of(Item, List) -> index_of(Item, List, 1).
 
 index_of(_, [], _)  -> not_found;
 index_of(Item, [Item|_], Index) -> Index;
-index_of(Item, [_|Tl], Index) -> index_of(Item, Tl, Index+1).  
+index_of(Item, [_|Tl], Index) -> index_of(Item, Tl, Index + 1).  
         
