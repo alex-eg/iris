49118ff8a95ac1568120a495acb621237befba54
taptap <alexey.e.egorov@gmail.com>
2013-09-25 08:19:41 +0400

    stub shrink module

--- src/modules/shrink.erl
+++ src/modules/shrink.erl
@@ -9,7 +9,20 @@ run(Args, _) ->
     [{shrinker, Config}] = gen_server:call(core, {get_config, shrinker}),
     Base = proplists:get_value(request_url, Config),
     Params = proplists:get_value(params, Config),
-    [URL, CustomID] = string:tokens(Args, " "),
+    Tokens = string:tokens(Args, " "),
+    parse(Tokens, Base, Params).
+
+parse([URL], Base, Params) ->
+    EncodedURL = http_uri:encode(URL),
+    {{_, 200, _}, _, Response} = gen_server:call(core, {get_http, 
+                                                        post, 
+                                                        {Base, [{"User-Agent", "curl/7.32.0"}], 
+                                                         "application/x-www-form-urlencoded", 
+                                                         io_lib:format(Params, [EncodedURL, ""])}, 
+                                                        [], []}),
+    Dom = mochiweb_html:parse(Response),
+    "~";    
+parse([URL, CustomID], Base, Params) ->
     EncodedURL = http_uri:encode(URL),
     {{_, 200, _}, _, Response} = gen_server:call(core, {get_http, 
                                                         post, 
