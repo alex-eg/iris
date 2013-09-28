8f85d311c9599b82d30dc681401dfa0c9899140e
taptap <alexey.e.egorov@gmail.com>
2013-09-28 22:20:19 +0400

    add stub log module

---
+++ src/log.erl
@@ -0,0 +1,38 @@
+-module(log).
+-behaviour(gen_server).
+-export([start_link/2]).
+-export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-include("xmpp.hrl").
+
+-record(state, 
+        {log_file,
+         name
+        }).
+
+start_link(LogFile, Name) ->
+    State = #state{
+               log_file = LogFile,
+               name = Name
+              },
+    gen_server:start_link({local, Name}, ?MODULE, State, []).
+   
+init(State) ->
+    {ok, State}.
+
+handle_call(_Any, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Any, State) ->
+    {noreply, State}.
+
+handle_info(_Any, State) ->
+    {noreply, State}.
+
+terminate(Reason, State) ->
+    ulog:info("Log server ~s stopped for the following reason: ~s",
+               [State#state.name, Reason]),
+    ok.
+
+code_change(_OldVersion, State, _Extra) ->
+    ulog:debug("Info log's code changed!"),
+    {ok, State}.
