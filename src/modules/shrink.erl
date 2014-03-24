b828184e4291a18d4a7ec299a1451858976a1817
Masterkatze <orange@nightmail.ru>
2014-03-24 20:17:16 +0600

    shrink module update

--- src/modules/shrink.erl
+++ src/modules/shrink.erl
@@ -4,31 +4,22 @@
 
 run("", _) ->
     "Nothing to shrink-shrink about!";
-
+    
 run(Args, _) ->
     [{shrinker, Config}] = gen_server:call(core, {get_config, shrinker}),
     Base = proplists:get_value(request_url, Config),
     Params = proplists:get_value(params, Config),
     Tokens = string:tokens(Args, " "),
     parse(Tokens, Base, Params).
-
+    
 parse([URL], Base, Params) ->
     EncodedURL = http_uri:encode(URL),
-    {{_, 200, _}, _, Response} = gen_server:call(core, {get_http, 
-                                                        post, 
-                                                        {Base, [{"User-Agent", "curl/7.32.0"}], 
-                                                         "application/x-www-form-urlencoded", 
-                                                         io_lib:format(Params, [EncodedURL, ""])}, 
+    {{_, 200, _}, _, ResponseJSON} = gen_server:call(core, {get_http,
+                                                        post,
+                                                        {Base, [{"User-Agent", "Maribel/1.0"}],
+                                                         "application/x-www-form-urlencoded",
+                                                         "fUrl=" ++ EncodedURL ++ "&id="},
                                                         [], []}),
-    Dom = mochiweb_html:parse(Response),
-    "~";    
-parse([URL, CustomID], Base, Params) ->
-    EncodedURL = http_uri:encode(URL),
-    {{_, 200, _}, _, Response} = gen_server:call(core, {get_http, 
-                                                        post, 
-                                                        {Base, [{"User-Agent", "curl/7.32.0"}], 
-                                                         "application/x-www-form-urlencoded", 
-                                                         io_lib:format(Params, [EncodedURL, CustomID])}, 
-                                                        [], []}),
-    Dom = mochiweb_html:parse(Response),
-    "nipah~".
+    {[{<<"result">>, {Response}}]} = jsonx:decode(list_to_binary(ResponseJSON)),
+    {_, Result} = lists:keyfind(<<"generated">>, 1, Response),
+    binary_to_list(Result).
