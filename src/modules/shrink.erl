92e05fa2062c04433ab6a0f90359593bdbef64fe
Masterkatze <orange@nightmail.ru>
2014-03-24 20:20:19 +0600

    oops: shrink module update

--- src/modules/shrink.erl
+++ src/modules/shrink.erl
@@ -4,14 +4,14 @@
 
 run("", _) ->
     "Nothing to shrink-shrink about!";
-    
+
 run(Args, _) ->
     [{shrinker, Config}] = gen_server:call(core, {get_config, shrinker}),
     Base = proplists:get_value(request_url, Config),
     Params = proplists:get_value(params, Config),
     Tokens = string:tokens(Args, " "),
     parse(Tokens, Base, Params).
-    
+
 parse([URL], Base, Params) ->
     EncodedURL = http_uri:encode(URL),
     {{_, 200, _}, _, ResponseJSON} = gen_server:call(core, {get_http,
@@ -23,3 +23,4 @@ parse([URL], Base, Params) ->
     {[{<<"result">>, {Response}}]} = jsonx:decode(list_to_binary(ResponseJSON)),
     {_, Result} = lists:keyfind(<<"generated">>, 1, Response),
     binary_to_list(Result).
+    
