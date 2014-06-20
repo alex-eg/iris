f80c84207d9078c375c870025c17d0e5ce065d70
taptap <alexey.e.egorov@gmail.com>
2014-06-21 00:01:54 +0400

    update config module to use new data structures

--- src/config.erl
+++ src/config.erl
@@ -1,7 +1,6 @@
 -module(config).
 -export([init/1, get_room_list/1, parse/2]).
 
--include_lib("exmpp/include/exmpp_client.hrl").
 -include("xmpp.hrl").
 
 init(Filename) ->
@@ -10,30 +9,21 @@ init(Filename) ->
     ConfigList.
 
 parse(jid_config, {jid_config, Config}) ->
-    %% Debug only
-    {_, Resource} = init:script_id(),
-    #jid_info {
-       jid = proplists:get_value(jid, Config),
-       %% resource = proplists:get_value(resource, Config),
-       resource = Resource,
-       status = proplists:get_value(status, Config),
-       password = proplists:get_value(password, Config),
-       rooms = proplists:get_value(rooms, Config),
-       modules = proplists:get_value(modules, Config)
-      }.
+    Jid = proplists:get_value(jid, Config),
+    Port = proplists:get_value(port, Config, 5222),
+    Resource = proplists:get_value(resource, Config),
+    Status = proplists:get_value(status, Config, "undefined"),
+    Password = proplists:get_value(password, Config),
+    RoomConfs = lists:map(fun(RoomCfg) ->
+                                  parse(room_config, RoomCfg)
+                          end,
+                          proplists:get_value(rooms, Config)),
+    Modules = proplists:get_value(modules, Config),
+    jid_config:create(Port, Jid, Status, Resource, Password, RoomConfs, Modules).
 
-get_room_list(#jid_info{rooms = RoomList}) ->
-    lists:map(
-      fun(RoomTuple) ->
-              if tuple_size(RoomTuple) == 2 ->
-                      {Room, Nick} = RoomTuple,
-                      {Room, Nick, nopassword};
-                 tuple_size(RoomTuple) == 3 ->
-                      {Room, Nick, Password} = RoomTuple,
-                      {Room, Nick, Password};
-                 true ->
-                      ulog:error("Bad Room Tuple ~p", [RoomTuple]),
-                      error
-              end
-      end,
-      RoomList).
+parse(room_config, {Jid, Config}) ->
+    Nick = proplists:get_value(nick, Config),
+    Modules = proplists:get_value(modules, Config),
+    Logging = proplists:get_value(logging, Config),
+    Banlist = proplists:get_value(banlist, Config),
+    room_config:create(Jid, Nick, Modules, Logging, Banlist).
