b273e9c56808207890ad8469fc087463cb22ede7
taptap <alexey.e.egorov@gmail.com>
2014-06-20 16:57:19 +0400

    fix syntax

--- src/message.erl
+++ src/message.erl
@@ -1,32 +1,25 @@
 -module(message).
 -export([create/1, get_type/1, get_body/1, get_from/1, get_timestamp/1]).
 
--record(message,
-        {type,
-         body,
-         from,
-         timestamp
-         }).
-
 create(RawMessage) ->
     Type = exmpp_message:get_type(RawMessage),
     From = format_str("~s", [exmpp_xml:get_attribute(RawMessage, <<"from">>, undefined)]),
     Body = format_str("~s", [exmpp_message:get_body(RawMessage)]),
     TimeStamp = calendar:local_time(),
-    #message{type = Type,
-             from = From,
-             body = Body,
-             timestamp = TimeStamp}.
+    #{type => Type,
+      from => From,
+      body => Body,
+      timestamp => TimeStamp}.
 
 get_type(Message) ->
-    Message#message.type.
+    maps:get(type, Message).
 
 get_body(Message) ->
-    Message#message.body.
+    maps:get(body, Message).
       
 get_from(Message) ->
-    Message#message.from.
+    maps:get(from, Message).
 
 get_timestamp(Message) ->
-    Message#message.timestamp.
+    maps:get(timestamp, Message).
     
