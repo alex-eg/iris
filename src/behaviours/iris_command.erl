a9c838c64d92eed9ea47ab023fe1f8b9d60e03de
taptap <alexey.e.egorov@gmail.com>
2014-06-23 05:25:13 +0400

    commented out iris_command behaviour for bedug purposes

--- src/behaviours/iris_command.erl
+++ src/behaviours/iris_command.erl
@@ -2,7 +2,9 @@
 
 %% subject to change
 -type chat_callback() :: fun(([char()], [char()]) -> [char()]).
--callback module_level() ->
-    'chat_command' | 'worker_module' | 'global_module'.
--callback get_hooks() ->
-    chat_callback() | fun((...) -> term()).
+-callback run(A :: term(), B :: term()) ->
+    term().
+%% -callback module_level() ->
+%%     'chat_command' | 'worker_module' | 'global_module'.
+%% -callback get_hooks() ->
+%%     chat_callback() | fun((...) -> term()).
