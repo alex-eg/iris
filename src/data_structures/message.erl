230178aea450718855a520e5909a12f964b780a2
taptap <alexey.e.egorov@gmail.com>
2014-06-26 01:40:01 +0400

    add from_room helper to message module; flatten list

--- src/data_structures/message.erl
+++ src/data_structures/message.erl
@@ -1,10 +1,10 @@
 -module(message).
--export([create/1, type/1, body/1, from/1, timestamp/1, raw/1]).
+-export([create/1, type/1, body/1, from/1, timestamp/1, raw/1, from_room/1]).
 
 create(RawMessage) ->
     Type = exmpp_message:get_type(RawMessage),
-    From = io_lib:format("~s", [exmpp_xml:get_attribute(RawMessage, <<"from">>, undefined)]),
-    Body = io_lib:format("~s", [exmpp_message:get_body(RawMessage)]),
+    [From] = io_lib:format("~s", [exmpp_xml:get_attribute(RawMessage, <<"from">>, undefined)]),
+    [Body] = io_lib:format("~s", [exmpp_message:get_body(RawMessage)]),
     TimeStamp = calendar:local_time(),
     #{type => Type,
       from => From,
@@ -23,6 +23,11 @@ body(Message) ->
       
 from(Message) ->
     maps:get(from, Message).
+
+from_room(Message) ->
+    From = from(Message),
+    [RoomJid|_] = string:tokens(From, "/"),
+    RoomJid.    
 
 timestamp(Message) ->
     maps:get(timestamp, Message).
