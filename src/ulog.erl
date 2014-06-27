9b0b4ff32c0f216a3f20d8cb0eb79bc970d15b72
taptap <alexey.e.egorov@gmail.com>
2014-06-28 01:30:59 +0400

    add named output functions

--- src/ulog.erl
+++ src/ulog.erl
@@ -1,12 +1,16 @@
 -module(ulog).
--export([debug/2, debug/1]).
--export([error/2, error/1]).
--export([info/1, info/2]).
--export([warning/1, warning/2]).
+-export([debug/3, debug/2, debug/1]).
+-export([error/3, error/2, error/1]).
+-export([info/3, info/2, info/1]).
+-export([warning/3, warning/2, warning/1]).
 
 out(Level, Format, Args) ->
     Record = io_lib:format(Format, Args),
     io:format("[~w] ~s~n", [Level, Record]).
+
+debug(From, Format, Args) ->
+    NewFormat = misc:format_str("<~s>: ~s", [From, Format]),
+    debug(NewFormat, Args).
 
 debug(Format, Args) ->
     out(dbg, Format, Args).
@@ -14,17 +18,29 @@ debug(Format, Args) ->
 debug(Args) ->
     io:format("[~w] ~ts~n",[dbg, Args]).
 
+error(From, Format, Args) ->
+    NewFormat = misc:format_str("<~s>: ~s", [From, Format]),
+    out(err, NewFormat, Args).
+
 error(Format, Args) ->
     out(err, Format, Args).
 
 error(Args) ->
     io:format("[~w] ~ts~n",[err, Args]).
 
+info(From, Format, Args) ->
+    NewFormat = misc:format_str("<~s>: ~s", [From, Format]),
+    info(NewFormat, Args).
+
 info(Format, Args) ->
     out(inf, Format, Args).
 
 info(Args) ->
     io:format("[~w] ~ts~n",[inf, Args]).
+
+warning(From, Format, Args) ->
+    NewFormat = misc:format_str("<~s>: ~s", [From, Format]),
+    warning(NewFormat, Args).
 
 warning(Record) ->
     out(wrn, "~p", [Record]).
