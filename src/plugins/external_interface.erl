3e34aad4d5ce8369a9cb1bc194d6e5836a007ce5
taptap <alexey.e.egorov@gmail.com>
2014-06-29 20:07:13 +0400

    return external_interface to gen_server form

--- src/plugins/external_interface.erl
+++ src/plugins/external_interface.erl
@@ -2,14 +2,13 @@
 %%-behavior(gen_server).
 -behavior(iris_plugin).
 -export([start/3, process_message/2]).
--export([shortcut/3, accept_loop/3]).
-%%-export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-export([start_link/2, accept_loop/4]).
+-export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 -record(state,
-        {supervisor,
-         parent_worker,
+        {parent_worker,
          port,
          socket,
          target_jid
@@ -18,106 +17,71 @@
 start(Supervisor, WorkerConfig, From) ->
     {ok, _Pid} = supervisor:start_child(Supervisor,
                                         {external_interface,
-                                         {?MODULE, shortcut, [Supervisor, WorkerConfig, From]},
+                                         {?MODULE, start_link, [WorkerConfig, From]},
                                          transient,
                                          5000,
                                          worker,
                                          [?MODULE]}).
 
-shortcut(Supervisor, WorkerConfig, From) ->
-    Config = jid_config:other_config(WorkerConfig),
+start_link(WorkerConfig, From) ->
+    Config = proplists:get_value(external_interface, jid_config:other_config(WorkerConfig)),
     Target = proplists:get_value(target_jid, Config),
     Port =  proplists:get_value(port, Config),
-    {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-    proc_lib:spawn(?MODULE, accept_loop, [LSocket, From, Target]).
+    State = #state{parent_worker = From,
+                   port = Port,
+                   target_jid = Target
+                  },
+    gen_server:start_link({local, external_interface}, ?MODULE, State, []).
 
-accept_loop(LSocket, Subscriber, Recepient) ->
+init(State) ->
+    Port = State#state.port,
+    {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+    NewState = State#state{socket = LSocket},
+    gen_server:cast(self(), wait_for_connection),
+    {ok, NewState}.
+
+handle_call(_Any, _From, State) ->
+    {noreply, State}.
+
+handle_cast(wait_for_connection, State) ->
+     proc_lib:spawn(?MODULE, accept_loop,
+                    [self(),
+                     State#state.socket,
+                     State#state.parent_worker,
+                     State#state.target_jid]),
+    {noreply, State};
+handle_cast(_Any, State) ->
+    {noreply, State}.
+
+handle_info(_Msg, State) ->
+    {noreply, State}.
+
+terminate(Reason, _State) ->
+    ulog:info("external_interface", "server terminated. Reason: ~p", [Reason]),
+    ok.
+
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
+
+%% gen_server callbacks end
+
+accept_loop(Parent, LSocket, Subscriber, Recepient) ->
+    ulog:debug("accept_loop", "started successfully!", []),
     {ok, Socket} = gen_tcp:accept(LSocket),
-    loop(Socket, Subscriber, Recepient).
+    loop(Socket, Subscriber, Recepient),
+    gen_server:cast(Parent, wait_for_connection),
+    exit(self(), shutdown).
 
 loop(Socket, Subscriber, Recepient) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Data} ->
             ulog:debug("external_interface", "recieved data via socket: ~p", [Data]),
             Message = binary_to_list(Data),
-            gen_server:cast(Subscriber, {send_message, binary_to_list(Data), Recepient}),
+            gen_server:cast(Subscriber, {send_message, Message, Recepient}),
             loop(Socket, Subscriber, Recepient);
         {error, closed} ->
+            ulog:debug("external_interface", "shutting down...", ""),
             ok
     end.
-
-%% start(Supervisor, WorkerConfig, From) ->
-%%     {ok, _Pid} = supervisor:start_child(Supervisor,
-%%                                         {external_interface,
-%%                                          {?MODULE, start_link, [Supervisor, WorkerConfig, From]},
-%%                                          transient,
-%%                                          5000,
-%%                                          worker,
-%%                                          [?MODULE]}).
-%% start_link(Supervisor, JidConfig, From) ->
-%%     Config = proplists:get_value(external_interface, jid_config:other_config(JidConfig)),
-%%     State = #state{supervisor = Supervisor,
-%%                    parent_worker = From,
-%%                    port = proplists:get_value(port, Config),
-%%                    target_jid = proplists:get_value(target_jid, Config)
-%%                   },
-%%     gen_server:start_link({local, external_interface}, ?MODULE, State, []).
-
-%% init(State) ->
-%%     Port = State#state.port,
-%%     {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-%%     NewState = State#state{socket = LSocket},
-%%     proc_lib:spawn(?MODULE, accept_loop, [self(), LSocket, State#state.parent_worker])
-%%     {ok, NewState}.
-
-%% handle_call(Any, _From, State) ->
-%%     ulog:debug("external_interface", "Recieved unknown request: ~p", [Any]),
-%%     {noreply, State}.
-
-%% handle_cast(accepted, State) ->
-%%     ulog:debug("external_interface", "got connection", []),
-    
-%% handle_cast(Any, State) ->
-%%     ulog:debug("external_interface", "Recieved unknown cast: '~p'", [Any]),
-%%     {noreply, State}.
-
-%% handle_info(Msg, State) -> 
-%%     ulog:debug("external_interface", "Recieved unknown message: '~p'", [Msg]),
-%%     {noreply, State}.
-
-%% terminate(Reason, _State) ->
-%%     ulog:info("External interface server terminated. Reason: ~p", [Reason]),
-%%     ok.
-
-%% code_change(_OldVersion, State, _Extra) -> {ok, State}.
-
-%% %% gen_server callbacks end
-
-%% accept_loop(Server, LSocket, Subscriber) ->
-%%     {ok, Socket} = gen_tcp:accept(LSocket),
-%%     gen_server:cast(Server, {accepted, self(), Subscriber}),
-%%     loop(Socket, Subscriber),
-%%     {normal, shutdown}.
-
-%% loop(Socket, Subscriber, Recepient) ->
-%%     case gen_tcp:recv(Socket, 0) of
-%%         {ok, Data} ->
-%%             ulog:debug("external_interface", "recieved data via socket: ~p", [Data]),
-%%             Message = binary_to_list(Data),
-%%             gen_server:cast(Subscriber, {send_message, binary_to_list(Data), Recepient}),
-%%             loop(Socket, Subscriber, Recepient);
-%%         {error, closed} ->
-%%             ok
-%%     end.
-
-%% handle_cast({new_post, Data}, State = #state{config = Config}) ->
-%%     Reciever = list_to_binary("maintenance@conference.anoma.ch"),
-%%     Sender = format_str("~s", [Config#jid_info.jid]),
-%%     Packet1 = exmpp_message:make_groupchat(?NS_JABBER_CLIENT, Data),
-%%     Packet2 = exmpp_xml:set_attribute(Packet1, <<"from">>, Sender),
-%%     Packet3 = exmpp_xml:set_attribute(Packet2, <<"to">>, Reciever),
-%%     gen_server:cast(self(), {send_packet, Packet3}),
-%%     {noreply, State};
 
 process_message(_Msg, _Cfg) ->
     nope.
