611a89b894fb9fc4e5df2b4e628488aff0fec08d
taptap <alexey.e.egorov@gmail.com>
2014-06-29 20:08:02 +0400

    fix jid_supervisor child name mess

--- src/jid_supervisor.erl
+++ src/jid_supervisor.erl
@@ -4,16 +4,15 @@
 -export([start_link/2]).
 
 start_link(Config, Name) ->
-    {ok, Pid} = supervisor:start_link({local, Name}, ?MODULE, [Config, Name]),
-    ulog:info("jid_supervisor ~p started and has pid ~p", [Name, Pid]),
-    {ok, Pid}.
+    {ok, _Pid} = supervisor:start_link({local, Name}, ?MODULE, [Config]).
 
-init([Config, Name]) ->
-    SupervisorName = list_to_atom(atom_to_list(Name) ++ "_supervisor"),
+init([Config]) ->
+    WorkerName = list_to_atom(jid_config:jid(Config)),
+    ulog:debug("starting jid_supervisor for ~p, process is ~p", [WorkerName, self()]),
     {ok, {
        {one_for_all, 1, 10},
-       [{SupervisorName,
-         {jid_worker, start_link, [Config, Name, self()]},
+       [{WorkerName,
+         {jid_worker, start_link, [Config, WorkerName, self()]},
          transient,
          10000,
          worker,
