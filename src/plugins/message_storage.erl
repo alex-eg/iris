9a8876205e865ba77d26970109fb32691ad33f15
taptap <alexey.e.egorov@gmail.com>
2014-08-10 15:36:01 +0400

    add message storage plugin

---
+++ src/plugins/message_storage.erl
@@ -0,0 +1,33 @@
+-module(user_message_storage).
+-behaviour(iris_plugin).
+
+-export([start/3, process_message/2]).
+
+start(_Parent, Config, _From) ->
+    ok.
+
+process_message(Message, Config) ->
+       Type = message:type(Message),
+    case Type of 
+        chat ->
+            process_chat(Message, Config);
+        groupchat ->
+            preprocess_groupchat(Message, Config);
+        error ->
+            ulog:warning(?MODULE, "got XMPP error stanza: ~p", [message:raw(Message)]);
+        _Other ->
+            ulog:error(?MODULE, "got unknown message type: ~s", [Type])
+    end.
+
+process_chat(Message, Config) ->
+    ok.
+
+preprocess_groupchat(Message, Config) ->
+    Stamp = exmpp_xml:get_element(message:raw(Message), delay), %% removing history messages
+    case Stamp of
+        undefined -> process_groupchat(Message, Config);
+        _ -> ok
+    end.
+
+process_groupchat(Message, Config) ->
+    ok.
