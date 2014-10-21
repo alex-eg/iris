873a7155c677351aa379d07a64c6d02493b93443
taptap <alexey.e.egorov@gmail.com>
2014-10-21 16:18:59 +0400

    remove debug output

--- src/config.erl
+++ src/config.erl
@@ -6,7 +6,6 @@
 get(Key, Proplist) when is_list(Proplist) ->
     proplists:get_value(Key, Proplist);
 get(Key, Ets) ->
-    lager:debug("Looking up ~w in ets #~w", [Key, Ets]),
     case ets:lookup(Ets, Key) of
         [] ->
             undefined;
