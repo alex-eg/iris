22499d34b1cf60129d9c51d685180253365fd117
taptap <alexey.e.egorov@gmail.com>
2015-07-08 14:57:26 +0300

    fix issue #35: check gelbooru.com availability

--- src/commands/gelb.erl
+++ src/commands/gelb.erl
@@ -4,10 +4,17 @@
 -behaviour(iris_command).
 
 run(_, _) ->
-    {_Status,
-     Headers,
-     _Body} = misc:httpc_request(get, {"http://gelbooru.com/index.php?page=post&s=random", []},
-                                 [{autoredirect, false}], 
-                                 []),
+    Response = misc:httpc_request(
+                 get,
+                 {"http://gelbooru.com/index.php?page=post&s=random", []},
+                 [{autoredirect, false}],
+                 []),
+    process_response(Response).
+
+process_response({Status, Headers, Body}) ->
     {_, URL} = lists:keyfind("location", 1, Headers),
-    "http://gelbooru.com/" ++ URL.
+    "http://gelbooru.com/" ++ URL;
+process_response({error, _Reason}) ->
+    "Sorry, could not retrieve information";
+process_response(Any) ->
+    lager:warning("Httpc request returned strange result: ~p", [Any]).
