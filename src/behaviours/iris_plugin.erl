183052955f38e15426c29167dd60ea522bae899e
taptap <alexey.e.egorov@gmail.com>
2015-08-05 13:18:21 +0300

    update plugin behaviour: add stop callback

--- src/behaviours/iris_plugin.erl
+++ src/behaviours/iris_plugin.erl
@@ -5,6 +5,11 @@
             From :: pid()) ->    %% jid_worker pid
     term().
 
--callback process_message(Message :: map(),   %% message map (also of data structures kind)
-                          Config :: map()) -> %% jid_config
+-callback process_message(Message :: map(),     %% message map (also of data structures kind)
+                          Config  :: map()      %% jid_config
+                          ) ->
+    term().
+
+-callback stop(From :: pid()                    %% jid worker pid
+                       ) ->
     term().
